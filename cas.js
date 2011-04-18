da7887a4210ac7cde9fd2065bc395ac9db3bcb7e
Bradley M. Froehle <brad.froehle@gmail.com>
2011-04-17 19:54:37 -0700

    Issue #889396 by Dane Powell, bfroehle: Login form compatibility with Rubik (Open Atrium) and similar themes.

--- cas.js
+++ cas.js
@@ -1,9 +1,8 @@
 
 Drupal.behaviors.cas = function (context) {
-  var $loginElements = $("#edit-name-wrapper, #edit-pass-wrapper, #user-login-form li, #user-login li");
-  var $casElements = $("#edit-cas-identifier-wrapper, li.uncas-link");
-  $("#edit-cas-identifier").hide();
-  $("#edit-cas-identifier-wrapper label").hide();
+  var $loginElements = $("#edit-name-wrapper, #edit-pass-wrapper, li.cas-link, li.openid-link");
+  var $casElements = $("li.uncas-link, .cas-login-redirection-message");
+  $("#edit-cas-identifier-wrapper").hide();
   if($("#edit-cas-identifier").attr("checked")) {
     $loginElements.hide();
     // Use .css("display", "block") instead of .show() to be Konqueror friendly.
@@ -18,9 +17,9 @@ Drupal.behaviors.cas = function (context) {
 
   $("li.cas-link", context)
     .click( function() {
-       $loginElements.hide();
-       $casElements.css("display", "block");
-	   $("#edit-cas-identifier").attr("checked", true);
+      $loginElements.hide();
+      $casElements.css("display", "block");
+      $("#edit-cas-identifier").attr("checked", true);
       // Remove possible error message.
       $("#edit-name, #edit-pass").removeClass("error");
       $("div.messages.error").hide();
@@ -28,13 +27,24 @@ Drupal.behaviors.cas = function (context) {
     });
   $("li.uncas-link", context)
     .click(function() {
-       $loginElements.css("display", "block");
-       $casElements.hide();
-	   $("#edit-cas-identifier").attr("checked", false);
+      $loginElements.css("display", "block");
+      // Fix OpenID compatibility.
+      $('li.user-link').hide();
+      $casElements.hide();
+      $("#edit-cas-identifier").attr("checked", false);
       // Clear cas Identifier field and remove possible error message.
       $("div.messages.error").css("display", "block");
       // Set focus on username field.
       $("#edit-name")[0].focus();
       return false;
+    });
+  // OpenID Compatibility
+  $("li.openid-link", context)
+    .click( function() {
+      $("li.cas-link").hide();
+    });
+  $("li.user-link", context)
+    .click( function() {
+      $("li.cas-link").css("display", "block");
     });
 };
